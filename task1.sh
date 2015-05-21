1173da3a77538d6b53b702de143a3b9fc5b73be9
praba1110 <praba1110@gmail.com>
2015-05-21 19:19:29 +0530

    Indented

--- task1.sh
+++ task1.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 for i in $(seq 1 100)
 do
-mkdir folder_$i
-chmod 700 folder_$i
-touch file_$i.txt
-mv file_$i.txt folder_$i
-
+	mkdir folder_$i
+	touch folder_$i/file_$i.txt
+	chmod 700 folder_$i
 done
